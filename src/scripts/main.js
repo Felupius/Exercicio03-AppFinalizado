bb0a4a183e53f175e4f7a5f29ab823dd1f0e5a3a
Felupius <gutryoside@gmail.com>
2025-03-30 21:21:29 -0300

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,87 @@
+var header = document.getElementById('header');
+var navigationHeader = document.getElementById('navigation_header');
+var showSidebar = false;
+
+function toggleSidebar() {
+    showSidebar = !showSidebar;
+    if (showSidebar) {
+        navigationHeader.style.marginLeft = '-10vw';
+        navigationHeader.style.animationName = 'showSidebar';
+    }
+    else {
+        navigationHeader.style.marginLeft = '-100vw';
+        navigationHeader.style.animationName = '';
+    }
+}
+
+function closeSidebar() {
+    if (showSidebar) {
+        showSidebar = true;
+        toggleSidebar();
+    }
+}
+
+window.addEventListener('resize', function (event) {
+    if (window.innerWidth > 820 && showSidebar) {
+        showSidebar = true;
+        toggleSidebar();
+    }
+});
+
+
+
+
+let slides = document.querySelectorAll('.slideshow');
+let dots = document.querySelectorAll('.dot');
+let slideIndex = 1;
+
+const showSlides = (n) => {
+    let i;
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        dots[i].setAttribute('class', 'dot');
+    }
+
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].setAttribute('class', 'dot ativo');
+    clearTimeout(timeoutID);
+    timeoutID = setTimeout(autoSlides, 4000);
+};
+
+const currentSlide = (n) => {
+    showSlides(slideIndex = n);
+};
+
+function autoSlides() {
+    let i;
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        dots[i].setAttribute('class', 'dot');
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].setAttribute('class', 'dot ativo');
+    timeoutID = setTimeout(autoSlides, 1000);
+}
+
+autoSlides();
